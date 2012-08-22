47d9b16b1626813bc9b244e29c89838e48772277
Jon Rodriguez <luciddreamworld@gmail.com>
2012-08-21 18:36:32 -0700

    start pulling tweets

--- Tweet2Speech/VLLMainViewController.m
+++ Tweet2Speech/VLLMainViewController.m
@@ -31,6 +31,30 @@
   // never returns
   
   NSLog(@"hello world");
+  
+  NSString *s1 = @"http://api.twitter.com/1/statuses/user_timeline.json?screen_name=brain_based_ai";
+  NSString *s2 = @"http://search.twitter.com/search.json?q=Vergence%20Labs&rpp=5&with_twitter_user_id=true&result_type=recent";
+  NSString *request_query = s1;
+  
+  // Do a simple search, using the Twitter API
+  TWRequest *request = [[TWRequest alloc] initWithURL:[NSURL URLWithString: request_query] 
+                                           parameters:nil requestMethod:TWRequestMethodGET];
+  
+  // Notice this is a block, it is the handler to process the response
+  [request performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error)
+  {
+    if ([urlResponse statusCode] == 200) 
+    {
+      // The response from Twitter is in JSON format
+      // Move the response into a dictionary and print
+      NSError *error;        
+      NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:responseData options:0 error:&error];
+      NSLog(@"Twitter response: %@", dict);                           
+    }
+    else
+      NSLog(@"Twitter error, HTTP response: %i", [urlResponse statusCode]);
+  }];
+  
 }
 
 - (void)viewDidUnload
